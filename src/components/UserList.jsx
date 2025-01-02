2cd63302b34ec43b0b722e301a89a71323527ad5
Adrian Linares <jalinaresm@ut.edu.co>
2025-01-01 22:19:09 -0500

    custom hook

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,36 +1,23 @@
-import { useEffect, useState } from "react";
+import { useFetchData } from "../hooks/useFetchData";
 import PropTypes from "prop-types";
 
 export const UserList = ({ endPoint }) => {
-    const [data, setData] = useState([]);
-
-    const fetchData = async () => {
-        try {
-            const response = await fetch(
-                `https://jsonplaceholder.typicode.com/${endPoint}`
-            );
-            const data = await response.json();
-            setData(data);
-        } catch (error) {
-            console.log(error);
-        }
-
-        UserList.propTypes = {
-            endPoint: PropTypes.string.isRequired,
-        };
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, [endPoint]);
-
+    const { data, isLoading } = useFetchData(endPoint);
     return (
         <>
             <ul>
-                {endPoint == "users"
-                    ? data.map((item) => <li key={item.id}>{item.name}</li>)
-                    : data.map((item) => <li key={item.id}>{item.email}</li>)}
+                {isLoading ? (
+                    <p>Cargando</p>
+                ) : endPoint == "users" ? (
+                    data.map((item) => <li key={item.id}>{item.name}</li>)
+                ) : (
+                    data.map((item) => <li key={item.id}>{item.email}</li>)
+                )}
             </ul>
         </>
     );
+};
+
+UserList.propTypes = {
+    endPoint: PropTypes.string.isRequired,
 };
